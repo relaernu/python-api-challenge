8af83b63d5a138ea9b9d8dcb96bdbd78baa7b229
relaernu <relaernu@gmail.com>
2021-01-14 23:12:53 +1100

    Remove keys

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e01d1c6b5d034740ab0fdbc0534f2d98"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyDg27vUXTsgV0YO6nw3uSyTkQIOj72ytAI"
+g_key = ""
